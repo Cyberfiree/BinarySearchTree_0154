3349398ad981c427311f7923b9da4fc57c2b898c
Cyberfire <buayagoreng99@gmail.com>
2024-06-13 11:44:36 +0800

    membuat variabel

--- BinarySearchTree_0154.cpp
+++ BinarySearchTree_0154.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
+//BinarySearchTree_4NimBelakang
+//Minimal 15 commit
+
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+};
 int main()
 {
 }
